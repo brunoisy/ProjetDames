271d6c77487ae0e1f55c9642d8355ef74b3f0fa3
Brunoob <brunodegomme@hotmail.com>
2015-03-02 18:24:01 +0100

    fonction new_game marche

--- dames.c
+++ dames.c
@@ -2,24 +2,40 @@
 #include <stdlib.h>
 #include "dames.h"
 
-int pnoir   =1;
-int pblanc  =5;
-int dnoir   =3;
-int dblanc  =7;
-int cvide   =0;
+const int cvide   =0;
+const int pnoir   =1;
+const int pblanc  =5;
+const int dnoir   =3;
+const int dblanc  =7;
+
 
 int main (int argc, char * argv[]){
-	
+	struct game * ng=new_game(10,10);
+	printf("xsize=%i\n", ng->xsize);
+	printf("ysize=%i\n",ng->ysize);
+	printf("cur_player=%i\n",ng->cur_player);
+	int ** board=ng->board;
+	printf("case 0 0=%i\n",board[0][0]);
+	printf("case 0 1=%i\n",board[0][1]);
+	printf("case 1 0=%i\n",board[1][0]);
+	printf("case 0 9=%i\n",board[0][9]);
+	printf("case 9 0=%i\n",board[9][0]);
 }
 
 
-extern struct game *new_game(int xsize, int ysize){ //pq xsize, ysize, pas tjrs 9x9?
-	struct game * newG;
-	newG->xsize=xsize;
-	newG->ysize=ysize;
-	int ** board = malloc(xsize*ysize*sizeof(int)); //louche
+extern struct game *new_game(int xsize, int ysize){ //pq xsize, ysize, pas tjrs 10x10?
+	struct game * newG=(struct game *) malloc(sizeof(struct game));
+
+	int ** board = (int **) malloc((size_t)xsize*sizeof(int *));
 	if (board==NULL)
 		{return NULL;}
+	int x;
+	for(x=0;x<xsize;x++){
+		board[x]=(int *) malloc((size_t)ysize*sizeof(int));
+		if (board[x]==NULL)
+			{return NULL;}
+	}
+		
 	//remplir le damier 
 	int i;
 	int j;
@@ -32,36 +48,42 @@ extern struct game *new_game(int xsize, int ysize){ //pq xsize, ysize, pas tjrs 
 				board[i][j]=pnoir;
 			}
 		}
-		j=0;
 	}
+	
 	for(i=4; i<6; i++){
 		for(j=0; j<10; j++){			
 			board[i][j]=cvide;			
 		}
-		j=0;
 	}
+
 	for(i=6; i<10; i++){
 		for(j=0; j<10; j++){
 			if ((i+j)%2==0){
-				board[i][j]=pblanc;
-			}
-			else{
 				board[i][j]=cvide;
 			}
+			else{
+				board[i][j]=pblanc;
+			}
 		}
-		j=0;
 	}
+
 	newG->board=board;
+	newG->xsize=xsize;
+	newG->ysize=ysize;
+	newG->moves=NULL; //pe un premier move vide? 
+	newG->cur_player=PLAYER_WHITE;
+
 	return newG;
 }
 
 
 
 extern struct game *load_game(int xsize, int ysize, const int **board, int cur_player){// utilisation 'extern' demandee ici?
-	struct game * newG;
-	newG->board=board;
+	struct game * newG=(struct game *) malloc(sizeof(struct game));	;
+	newG->board=(int **) board;
 	newG->xsize=xsize;
 	newG->ysize=ysize;
-	newG->cur_player=cur_player; //quid de struct move *moves dans la struct game? : pas nécessaire à l'initialisation
+	newG->moves=NULL; //idem newGame
+	newG->cur_player=cur_player;
 	return newG;
 }
